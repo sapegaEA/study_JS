f351859b46bba55250cf19c0eeed46b21e17e781
Evgeniy Sapega <sapega93@gmail.com>
2020-06-06 23:36:40 +0300

    Сделано задание 2

--- script/script.js
+++ script/script.js
@@ -1,9 +1,17 @@
-let money = 1;
-let income = 2;
-let addExpenses = 3;
-let deposit = 4;
-let mission = 5;
-let period = 6;
+let money = 60000;
+let income = 'Фриланс';
+let addExpenses = 'Покупки, интернет, коммуналка';
+let deposit = true;
+let mission = 150000;
+let period = 7;
+let budgetDay = money/30;
 
-alert('Hello');
-console.log('Привет, мир');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период раверн ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log(budgetDay);
