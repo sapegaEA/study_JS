e0921ee9cb1a6b876999a6cbc9d9dbb090d679fa
Evgeniy Sapega <sapega93@gmail.com>
2020-06-07 04:17:18 +0300

    Сделано усложненное задание lesson02

--- script/script.js
+++ script/script.js
@@ -1,17 +1,11 @@
-let money = 60000;
-let income = 'Фриланс';
-let addExpenses = 'Покупки, интернет, коммуналка';
-let deposit = true;
-let mission = 150000;
-let period = 7;
-let budgetDay = money/30;
+let num = 266219;
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
-console.log(budgetDay);
+let arr = num.toString().split('');
+let result = arr.reduce(function(sum, current) {
+    return sum * current;
+  });
+console.log(result);
+
+let exp = result ** 3;
+
+alert(exp.toString().slice(0, 2)); 
